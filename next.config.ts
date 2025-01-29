0b277048a50850b2699febf0cffec2b117800784
The-Orange-Dot <theorangedottwitch@gmail.com>
2025-01-29 08:23:33 -0500

    added remotePatterns to config to convert jpg/png to webp

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,31 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "/**",
+        search: "",
+      },
+      {
+        protocol: "http",
+        hostname: "192.168.86.32",
+        port: "3000",
+        pathname: "/**",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "qsg40ooowk044gsokgcww840.hung-le.com",
+        port: "",
+        pathname: "/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
